90e4a98d6dbee2cb0d1a9f9f2154230100724edb
MuskanNadeem55 <160000879+MuskanNadeem55@users.noreply.github.com>
2024-03-24 10:24:11 -0700

    changes updated

---
+++ index.js
@@ -0,0 +1,17 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+const randNum = Math.floor(Math.random() * 6 + 1);
+const result = await inquirer.prompt([
+    {
+        message: "Please guess the number between 1 to 6: ",
+        type: "number ",
+        name: "userGuessedNum",
+    },
+]);
+//Conditional statement
+if (result.userGuessedNum === randNum) {
+    console.log("Congratulations! You entered the correct number");
+}
+else {
+    console.log("Wrong, Please try again!");
+}
